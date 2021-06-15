9c7acbd6584a0a865ca1e08ef70ac7051936d2c9
Dimas Neto <dimasneto@pop-os.localdomain>
2021-06-15 19:36:47 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AuthProvider } from '../contexts/AuthContext';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
+
+export default MyApp
+
+
